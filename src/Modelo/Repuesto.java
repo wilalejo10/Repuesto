754467325eefa9f4d8f111db00435c63b1b04fc4
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-26 11:39:54 -0500

    Update Repuesto.java

--- src/Modelo/Repuesto.java
+++ src/Modelo/Repuesto.java
@@ -21,23 +21,6 @@ public class Repuesto
     }
     //SET ASIGNA
     //GET DEVUELVE
-/*
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-*/
     public int getStock() {
         return stock;
     }
