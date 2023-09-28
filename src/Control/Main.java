aa17a2c9be3662a43a914b5ef345c59c2cd2b7b5
ALEJANDRO RINCON <126520302+wilalejo10@users.noreply.github.com>
2023-09-28 09:15:14 -0500

    Update Main.java

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -3,36 +3,34 @@ package Control;
 import Modelo.Empresa;
 import Vista.InOut;
 
-public class Main
-{
-    public static void main(String [] args){
-        Empresa Em= new Empresa();
-        InOut ob=new InOut();
-        int opcion=0;
+public class Main {
+    public static void main(String[] args) {
+        Empresa Em = new Empresa();
+        InOut ob = new InOut();
+        int opcion = 0;
 
-        do{
-            try{
-                opcion=ob.solicitarEnteros("""
-                                         1.-Comprar repuestos
-                                         2.-Mostrar el repuesto más vendido
-                                         3.-Mostrar el total recaudado
-                                         4.-SALIR
-                                         INGRESE SU OPCION""");
-                switch(opcion){
+        do {
+            try {
+                opcion = ob.solicitarEnteros("""
+                        1.-Comprar repuestos
+                        2.-Mostrar el repuesto más vendido
+                        3.-Mostrar el total recaudado
+                        4.-SALIR
+                        INGRESE SU OPCION""");
+                switch (opcion) {
                     case 1 -> {
                         Em.registrarRepuestos();
                     }
                     case 2 -> {
-
+                        Em.mostrarRepuestoMasVendido();
                     }
-                    case 3-> {
+                    case 3 -> {
                         Em.mostrarInformacionCompras();
                     }
                 }
-            }catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 ob.mensaje("Error");
             }
-        }while(opcion != 4);
-
+        } while (opcion != 4);
     }
 }
